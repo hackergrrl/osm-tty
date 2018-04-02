be9364fd83bc4b0bd00d85c1eaa75884a45983b8
Stephen Whitmore <sww@eight45.net>
2018-04-02 13:15:59 +0200

    better horizontal scaling

--- render.js
+++ render.js
@@ -12,7 +12,7 @@ function renderElement (charm, camera, allElements, element) {
 }
 
 function nodeToTermCoords (camera, node) {
-  var horizScale = 1 //0.5
+  var horizScale = 0.55
   var vertScale = 1
 
   var w = camera.bbox[1][1] - camera.bbox[1][0]
