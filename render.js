65dc02ff6204dbbf5ab985d108a223cc79823b8c
Stephen Whitmore <sww@eight45.net>
2018-04-08 19:22:21 +0200

    fix: more hint chars

--- render.js
+++ render.js
@@ -222,7 +222,8 @@ function drawLabel (screen, x, y, label) {
   screen.write(label)
 }
 
-var hintchars = [ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ]
+var hintchars = [ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+                  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=' ]
 function genHint (hints) {
   return hintchars[Object.keys(hints).length % hintchars.length]
 
